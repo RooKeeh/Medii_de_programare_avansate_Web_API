780dd84c65f12377013e1b8a4ec3452947f728a9
Andrei Moldovan <andreimoldovan6@gmail.com>
2023-10-26 14:45:20 +0300

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moldovan_Andrei_Lab6.Models;
+
+namespace Moldovan_Andrei_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
